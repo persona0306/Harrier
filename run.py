2178c4fd177662916732752b40586b0d7aaf4f9e
persona0306 <omgitspersona0306@gmail.com>
2025-10-03 20:27:42 +0900

    Initial Commit

---
+++ run.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+if __name__ == '__main__':
+    if not TOKEN:
+        raise ValueError("DISCORD_BOT_TOKEN environment variable not set.")
+    bot.run(TOKEN)
